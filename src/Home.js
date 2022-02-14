937fa812b48b8bbb1cb4d535dd26a91609400839
eine3131 <tsiresy@gmail.com>
2022-02-14 16:57:33 +0100

    Making a Custom Hook

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,33 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
-
+    const [error, setError] = useState(null);
     useEffect(() => {
         setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
+            fetch('http://localhost:8000/blogss')
                 .then(res => {
+                    // console.log(res);
+                    if (!res.ok) {
+                        throw Error('Could not fetch the data for that resource');
+                    }
                     return res.json();
                 })
                 .then(data => {
+                    setError(null);
                     setBlogs(data);
                     setIsPending(false);
-                });
+                })
+                .catch(err => {
+                    // console.log(err.message); 
+                    setIsPending(false);
+                    setError(err.message);
+                })
         }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {error && <div> {error} </div>}
             {isPending && <div> Loading... </div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
