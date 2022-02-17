e7b30448237273355390e37f6fbfde15f21cd310
eine3131 <tsiresy@gmail.com>
2022-02-17 09:50:22 +0100

    Router Links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+
 function App() {
   return (
     <Router>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1> The Dojo Blog </h1>
             <div className="links">
-                <a href="/"> Home </a>
-                <a href="/Create"> New Blog </a>
+                <Link to="/"> Home </Link>
+                <Link to="/create"> New Blog </Link>
             </div>
         </nav>
     );
